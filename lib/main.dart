41832cd9a17993501e78e68d84196f2250e5482c
Ahmad <muahmad710@gmail.com>
2022-09-05 00:50:46 +0500

    Now we going to comsats

--- lib/main.dart
+++ lib/main.dart
@@ -29,16 +29,16 @@ class MapSampleState extends State<MapSample> {
   );
 
   static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
+      bearing: 210.8334901395799,
+      target: LatLng(33.651592, 73.156456),
+      tilt: 70.440717697143555,
       zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.satellite,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
